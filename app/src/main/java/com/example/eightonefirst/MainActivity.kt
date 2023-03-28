3c69db0b31d19c489059538d63319fb93ac24598
sirojiddinbakhtiyorovich <msirojiddin62@gmail.com>
2023-03-28 03:06:26 -0700

    Commentariya yozdim

--- app/src/main/java/com/example/eightonefirst/MainActivity.kt
+++ app/src/main/java/com/example/eightonefirst/MainActivity.kt
@@ -10,5 +10,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        // TODO: SirojiddinMadaminov
+
+
+        //commit
+        //pull
+        //push
     }
 }
